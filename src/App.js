7dd123d03153871745a02bb053848d93c73112cf
ashutosh887 <ashutoshj887@gmail.com>
2022-04-14 10:31:31 +0530

    links added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import "./App.css";
+import Links from "./Links";
 import logo from "./logo.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Actions - By Ashutosh</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href="http://ashutosh887.me/" target="_blank">
+          <img src={logo} className="App-logo" alt="logo" />
         </a>
+        <p>Socials - By Ashutosh</p>
+        <Links />
       </header>
     </div>
   );
---
+++ src/Links.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Links = () => {
+  return (
+    <>
+      <a
+        className="App-link"
+        href="https://github.com/ashutosh887"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
+      <a
+        className="App-link"
+        href="https://www.linkedin.com/in/ashutosh887/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        LinkedIn
+      </a>
+      <a
+        className="App-link"
+        href="https://twitter.com/ashutoshj887"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Twitter
+      </a>
+    </>
+  );
+};
+
+export default Links;
